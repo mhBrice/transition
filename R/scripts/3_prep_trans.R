eb6cca16b6c44ec6de693d499ce93b1ca764e7b7
mhBrice <mariehbrice@outlook.com>
2019-08-29 12:46:26 -0400

    maps

--- R/scripts/3_prep_trans.R
+++ R/scripts/3_prep_trans.R
@@ -14,35 +14,32 @@ states_ba <- states_ba %>%
   mutate(year_measured = year_measured - 1970) %>% 
   mutate_at(vars(EPMATORG:age_mean, aTP:GSlength), scale)
 
+sc_sTP <- c(attr(states_ba$sTP, "scaled:center"), attr(states_ba$sTP, "scaled:scale"))
+
 # Select and subset
 
 states_ba <- states_ba %>% 
   select(ID_PE:states_num6, 
-         "sTP", "GSlength", "sPP", "CMI",
-         "natural", "logging", "disturb", 
-         "natural_lag", "logging_lag", "disturb_lag", 
-         "TYPEHUMUS",  "DRAIN", "ecoreg3") %>% 
-  na.omit() %>% mutate_if(is.matrix, as.vector) %>%
-  filter(TYPEHUMUS %in% c("MU", "MD", "MR", "SO", "TO")) %>% # Subset plots for humus type
-  mutate(TYPEHUMUS = droplevels(TYPEHUMUS)) #%>%
-  #group_by(ID_PE) %>% filter(n() > 1)
+         "sTP", "CMI",
+         "natural", "logging", 
+         #"natural_lag", "logging_lag", 
+         "DRAIN","PH_HUMUS", "ecoreg3", "ecoreg6") %>% 
+  na.omit() %>% mutate_if(is.matrix, as.vector) %>% 
+  filter(complete.cases("DRAIN")) %>%
+  group_by(ID_PE) %>% filter(n() > 1) %>% ungroup()
 
-# Remove test data (fifth inventory)
-train_dat <- filter(states_ba, year_measured+1970 <= 2015) %>%
-  group_by(ID_PE) %>% filter(n() > 1)
-test_dat <- filter(states_ba, year_measured+1970 >= 2015) %>%
-  filter(ID_PE %in% train_dat$ID_PE)
- 
+states_ba$states_num = states_ba$states_num75
 
-# State transition matrix to compute transition matrix property
+states_ba$states = states_ba$states_ba75
 
-# states_trans <- states_ba %>%
-#   group_by(plot_id) %>%
-#   mutate(year1 = year_measured + 1970) %>%
-#   mutate(year2 = lead(year_measured, 1L) + 1970) %>%
-#   mutate(time_interv = year2 - year1) %>%
-#   mutate(From = states_ba) %>%
-#   mutate(To = lead(states_ba, 1L)) %>%
-#   mutate(transition = paste0(From, "->", To)) %>%
-#   filter(!is.na(year2)) %>%
-#   select(ID_PE:plot_id, year1:transition, disturb, natural, logging)
+ecoregion <- st_read("data/ecoregion_simple.gpkg")
+ecoregion$SOUS_DOM6 <- factor(ecoregion$SOUS_DOM6, c("Sugar maple-bitternut hickory",
+                                                     "Sugar maple-basswood",
+                                                     "Sugar maple-yellow birch",
+                                                     "Balsam fir-yellow birch",
+                                                     "Balsam fir-white birch",
+                                                     "Spruce-moss"))
+xy <- st_read("data/plot_xy32198_may2018.gpkg") %>%
+  st_transform(st_crs(ecoregion)) %>% 
+  filter(plot_id %in% states_ba$plot_id)
+
--- R/scripts/fig1_map.R
+++ R/scripts/fig1_map.R
@@ -3,47 +3,36 @@
 ##################################
 
 library(sf)
-require(RColorBrewer)
-require(scales)
+library(RColorBrewer)
+library(scales)
 
-reg_title <- c("Sugar maple-hickory\nSugar maple-basswood",
+source('R/functions/plot_map.R')
+
+### DATA ####
+
+source('R/scripts/3_prep_trans.R')
+
+reg_title <- c("Sugar maple-hickory &\nSugar maple-basswood",
                "Sugar maple-yellow birch",
                "Balsam fir-yellow birch",
                "Balsam fir-white birch", "Spruce-moss")
 
 col_reg <- brewer.pal(6,"Spectral")[c(1,1:3,5,6)]
 
-n_reg <- table(BCDdf$ecoreg5)
+n_reg <- table(states_ba$ecoreg6)
 
-lim <- st_bbox(ecoregion)
 
-pdf("ms/figures/fig1_region.pdf",
-    width = 5, height = 3)
-# quartz(width = 5, height = 3)
-par(mar=c(1.8,2.3,.3,0.3))
+pdf("res/fig1_region.pdf", width = 4.3, height = 2.4)
+# quartz(width = 4.3, height = 2.4)
+par(mar=c(1.5,2.1,.3,0.3))
 
-plot0(xlim = lim[c(1,3)]+c(.3,.1), ylim = lim[c(2,4)]+c(-.1,.1),
-      grid.col = alpha("grey60", .3), fill = "white")
+plot_map(ecoregion, col_reg = alpha(col_reg,.4), xy_pts = xy)
 
-box2(1:2)
-plot(st_geometry(ecoregion), border = "grey50",
-     col = alpha(col_reg,.4)[ecoregion$SOUS_DOM6],
-     axes=F, add=T)
-
-axis(1, labels = F, tcl = -.4)
-axis(1, at = seq(-80,-60,by=5), labels = paste0(abs(seq(-80,-60,by=5)), "°W"),
-     line = -.3, cex.axis = .8, tick = F)
-axis(2, labels = F, tcl = -.4)
-axis(2, at = seq(46,52,by=2), labels = paste0(seq(46,52,by=2), "°N"),
-     line = -.3, las = 1, cex.axis=.8, tick = F)
-
-plot(st_geometry(xy), cex = .1, pch = 19, col = alpha("grey15",.3), add = T)
-
-points(-61.8, 47.5, cex= 3, pch = 19, col = "white")
-text(-59.7, 53.1, paste0(reg_title[5], " (", n_reg[5], ")"), cex = .75, xpd = NA)
-text(-59.2, 49.6, paste0(reg_title[4], "\n(", n_reg[4], ")"), cex = .75, xpd = NA)
-text(-63.8, 47.5, paste0(reg_title[3], " (", n_reg[3], ")"), cex = .75)
-text(-64.9, 46.5, paste0(reg_title[2], " (", n_reg[2], ")"), cex = .75)
-text(-66.8, 45.2, paste0(reg_title[1], " (", n_reg[1], ")"), cex = .75)
+points(-61.8, 47.4, cex = 3, pch = 15, col = "white")
+text(-59.4, 53.15, paste0(reg_title[5], " (", n_reg[6], ")"), cex = .7, xpd = NA)
+text(-59.1, 49.55, paste0(reg_title[4], "\n(", n_reg[5], ")"), cex = .7, xpd = NA)
+text(-63.4, 47.5, paste0(reg_title[3], " (", n_reg[4], ")"), cex = .7)
+text(-64.5, 46.5, paste0(reg_title[2], " (", n_reg[3], ")"), cex = .7)
+text(-66.4, 45.2, paste0(reg_title[1], " (", n_reg[2]+n_reg[1], ")"), cex = .7)
 
 dev.off()
