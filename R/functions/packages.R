7a6b35fd2351c7c7709040b54b2225a34f8bea9d
mhBrice <mariehbrice@outlook.com>
2019-11-29 12:05:15 -0500

    function to load all packages

---
+++ R/functions/packages.R
@@ -0,0 +1,22 @@
+### Load all packages
+
+library(msm)
+
+library(diagram)
+library(graphicsutils)
+
+library(scales)
+library(RColorBrewer)
+
+library(data.table)
+library(dplyr)
+library(reshape2)
+library(sf)
+
+library(knitr)
+library(kableExtra)
+library(latex2exp)
+
+#library(DescTools)
+
+library(scoring)
