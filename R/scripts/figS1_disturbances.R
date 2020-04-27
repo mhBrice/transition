7893eb06c5b0ebaed4d023047dacf64358c2ac8d
mhBrice <mariehbrice@outlook.com>
2020-04-27 11:17:07 -0400

    figure resolution

--- R/scripts/figS1_disturbances.R
+++ R/scripts/figS1_disturbances.R
@@ -4,44 +4,7 @@
 
 source("R/functions/packages.R")
 
-my_waffle <- function(x, nrows, ncols = 65, 
-                      pal = "Dark2", main = NULL, lgd = TRUE, ...) {
-  nd <- length(x)
-  if(nd != 1) {
-    cols <- brewer.pal(length(x), name = pal)
-  } else {
-    cols <- brewer.pal(3, name = pal)[1]
-  }
-  xx <- rep(cols, times = x)
-  lx <- length(xx)
-  
-  m <- matrix(nrow = nrows, ncol = ncols)
-  m[1:length(xx)] <- xx
-  
-  o <- cbind(c(row(m)), c(col(m))) + 1
-  plot0(ylim = c(0, max(o[, 2]) + 1), xlim = c(0, max(o[, 1]) + 1),
-        asp = 1, xaxs = 'i', yaxs = 'i')
-  
-  mtext(main, 3, line = .85, cex = .8, font = 2)
-  mtext(paste("n =", sum(x)), 3, line = 0, cex = .65)
-  usr <- par("usr")
-  rect(ybottom=o[, 2], 
-       xleft=o[, 1], 
-       ytop=o[, 2] + .85, 
-       xright=o[, 1] + .85, 
-       col = c(m), border = NA)
-  
-  if(lgd) {
-    lgd_pos <- which(is.na(m), arr.ind = T)[1,2]
-    legend(lgd_pos+1, mean(usr[3:4]), yjust = 0.5, cex = .8,
-           legend = paste0(names(x), " ", round(x/sum(x)*100, 2), "%"), 
-           fill = cols, border = NA,
-           bty = "n", x.intersp = .7, y.intersp = 1,
-           inset = c(0, -1), xpd = NA)
-  }
-  invisible(list(m = m, o = o))
-}
-
+source("R/functions/my_waffle.R")
 
 ### DATA ####
 
@@ -63,113 +26,6 @@ env_all1 <- env_all %>%
 reg <- aggregate(env_all1$ID_PE_MES, by = list(env_all1$ecoreg3), length)
 
 ### Frequency of disturbances ####
-
-disturb_summ <- env_all %>% 
-  group_by(ID_PE) %>% 
-  summarise("Minor" = all(natural==0 & logging==0),
-            "Moderate natural" = any(natural==1),
-            "Major natural" = any(natural==2),
-            "Moderate logging" = any(logging==1),
-            "Major logging" = any(logging==2)) 
-
-disturb_summ <- apply(disturb_summ[,-1], 2, sum)
-
-
-
-### Frequency of the 21 original disturbance types ####
-
-log2 <- table(env_all$ORIGINE, env_all$logging)[,3][c("CBA", "CBT", "CPR", "CT")]
-nat2 <- table(env_all$ORIGINE, env_all$natural)[,3][c("BR", "ES", "CHT","DT")]
-
-log1 <- table(env_all$PERTURB, env_all$logging)[,2][c("CAM","CB","CD","CDL","CE","CJ","CP","EPC")]
-nat1 <- table(env_all$PERTURB, env_all$natural)[,2][c("BRP", "EL", "CHP", "VEP", "DP")]
-
-disturb_types <- list("No or minor" = disturb_summ[1],
-                      "Moderate natural" = nat1,
-                      "Major natural" = nat2,
-                      "Moderate logging" = log1,
-                      "Major logging" = log2)
-
-disturb_types <- lapply(disturb_types, function(x) sort(x, decreasing = TRUE))
-lapply(disturb_types,sum)
-
-disturb_code <- list(c("Outbreak", "Windfall", "Decline", "Burn", "Ice storm"),
-                  c("Burn", "Outbreak", "Windfall", "Decline"),
-                  c("Partial cut", "Commercial thinning", "Strip cut", 
-                    "Selection cut", "Diameter-limit cut", 
-                    "Partial cut + outbreak", "Improvement cut", "Checkerboard cut"),
-                  c("Clearcut", "Cut with protection\nof regeneration",
-                  "Final strip cut", "Strip cut"))
-
-
-
-
-### FIGURE S2. WAFFLE PLOTS ####
-
-lgd = c(FALSE, TRUE, TRUE, TRUE, TRUE)
-
-png("res/figS2_waffle.png", width = 7.4, height = 7.3, units = "in", res = 300)
-#quartz(width = 7.4, height = 7.3)
-layout(matrix(c(1,1,2:5), 3, byrow = TRUE))
-par(oma=c(0,0,0,1))
-for(i in 1:length(disturb_types)) {
-  d <- disturb_types[[i]]
-  if(i!=1) names(d) <- disturb_code[[i-1]]
-  ncols <- ifelse(i==1, 100, 65)
-
-  par(mar = c(0,0,1.8,0))
-  if(i==1) par(mar = c(0,0,1.8,22)) 
-  if(i %in% c(2,4)) par(mar = c(0,0,1.8,5.8))
-  
-  my_waffle(d, nrows = 50, ncols = ncols,
-            main = names(disturb_types[i]), lgd = lgd[i])
-
-}
-
-dev.off()
-
-########################
-#### TEST ######
-########################
-
-my_waffle <- function(x, nrows, ncols = 65, 
-                      pal = "Dark2", main = NULL, lgd = TRUE, ...) {
-  nd <- length(x)
-  if(nd != 1) {
-    cols <- brewer.pal(length(x), name = pal)
-  } else {
-    cols <- brewer.pal(3, name = pal)[1]
-  }
-  xx <- rep(cols, times = x)
-  lx <- length(xx)
-  
-  m <- matrix(data = c(xx, rep(NA,(nrows*ncols-length(xx)))), 
-              nrow = nrows, ncol = ncols, byrow = T)
-  
-  o <- cbind(c(row(m)), c(col(m))) + 1
-  plot0(xlim = c(0, max(o[, 2]) + 1), ylim = c(0, max(o[, 1]) + 1),
-        asp = 1, xaxs = 'i', yaxs = 'i')
-  
-  mtext(main, 3, line = .85, cex = .8, font = 2)
-  #mtext(paste("n =", sum(x)), 3, line = 0, cex = .65)
-  usr <- par("usr")
-  rect(xleft=o[, 2], 
-       ybottom=o[, 1], 
-       xright=o[, 2] + .85, 
-       ytop=o[, 1] + .85, 
-       col = c(m), border = NA)
-  
-  if(lgd) {
-    lgd_pos <- which(is.na(m), arr.ind = T)[1,2]
-    legend(lgd_pos+1, mean(usr[3:4]), yjust = 0.5, cex = .8,
-           legend = names(x), #paste0(names(x), " ", round(x/sum(x)*100, 2), "%"), 
-           fill = cols, border = NA,
-           bty = "n", x.intersp = .7, y.intersp = 1,
-           inset = c(0, -1), xpd = NA)
-  }
-  
-}
-
 
 disturb_summ <- env_all %>% 
   group_by(ID_PE, ecoreg3) %>% 
@@ -197,13 +53,7 @@ disturb_types <- list("No or minor" = disturb_summ[,2],
                       "Moderate logging" = log1,
                       "Major logging" = log2)
 
-sort_col <- function(x) {
-  if(is.null(dim(x))) {
-    x
-  }else{
-    x[,order(colSums(x), decreasing = TRUE)]} 
-}
-  
+
 disturb_types <- lapply(disturb_types, sort_col)
 lapply(disturb_types,sum)
 
@@ -216,11 +66,16 @@ disturb_code <- list(c("Outbreak", "Windfall", "Decline", "Burn", "Ice storm"),
                        "Final strip cut", "Strip cut"))
 
 
+disturb_col <- list("No or minor" = "#c7cbce",
+                    "Moderate natural" = c("#3c784f", "#89a8ad", "#68566e", "#a83f02", "#3c7dba"),
+                    "Major natural" = c("#3c784f","#a83f02", "#89a8ad", "#68566e"),
+                    "Moderate logging" = c("#4d8585", gpuPalette('insileco', 13)[3:9]),
+                    "Major logging" = c("#4d8585", gpuPalette('insileco', 13)[c(8,4,4)]))
 
 
 ### FIGURE S1. WAFFLE PLOTS ####
 
-png("res/figS1_waffle.png", width = 5.5, height = 6.5, units = "in", res = 300)
+png("res/figS1_waffle.png", width = 5.5, height = 6.5, units = "in", res = 600)
 
 #quartz(width = 5.5, height = 6.5)
 par(oma=c(0,0,0,0))
@@ -245,7 +100,7 @@ for(i in 1:length(disturb_types)) {
     }else { 
       dr <- d[r,]
       }
-    my_waffle(dr, nrows = nrows, ncols = 40,
+    my_waffle(dr, nrows = nrows, ncols = 40, cols = disturb_col[[i]],
               main = "", lgd = FALSE)
 
   }
@@ -254,7 +109,7 @@ for(i in 1:length(disturb_types)) {
   if(i>1)
     legend(-1, 0, xjust = 0, yjust = .5, cex = .8,
            legend = rev(names(dr)), 
-           fill = rev(brewer.pal(length(dr), name = "Dark2")), border = NA,
+           fill = rev(disturb_col[[i]]), border = NA,
            bty = "n", x.intersp = .7, y.intersp = 1,
            inset = c(0, -1), xpd = NA)
   
